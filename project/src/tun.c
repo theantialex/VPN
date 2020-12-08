8dc6cc0c9525014a938169e7f85f295731e58c1b
Alexandra <saxara_a@mail.ru>
2020-12-08 18:55:51 +0300

    Cleaned tun.c

--- project/src/tun.c
+++ project/src/tun.c
@@ -18,10 +18,7 @@
 #include "tun.h"
 
 /* buffer for reading from tun/tap interface, must be >= 1500 */
-#define BUFSIZE 2000   
-#define CLIENT 0
-#define SERVER 1
-#define PORT 55555
+#define BUFSIZE 2000 
 
 /* some common lengths */
 #define IP_HDR_LEN 20
@@ -30,6 +27,33 @@
 
 int debug;
 char *progname;
+
+/**************************************************************************
+ * do_debug: prints debugging stuff (doh!)                                *
+ **************************************************************************/
+void do_debug(char *msg, ...){
+  
+  va_list argp;
+  
+  if (debug){
+    va_start(argp, msg);
+    vfprintf(stderr, msg, argp);
+    va_end(argp);
+  }
+}
+
+/**************************************************************************
+ * my_err: prints custom error messages on stderr.                        *
+ **************************************************************************/
+void my_err(char *msg, ...) {
+
+  va_list argp;
+  
+  va_start(argp, msg);
+  vfprintf(stderr, msg, argp);
+  va_end(argp);
+}
+
 
 /**************************************************************************
  * tun_alloc: allocates or reconnects to a tun/tap device. The caller     *
@@ -69,98 +93,6 @@ error:
 	return FAILURE;
 }
 
-/**************************************************************************
- * cread: read routine that checks for errors and exits if an error is    *
- *        returned.                                                       *
- **************************************************************************/
-// int cread(int fd, char *buf, int n){
-  
-//   int nread;
-
-//   if((nread=read(fd, buf, n))<0){
-//     perror("Reading data");
-//     exit(1);
-//   }
-//   return nread;
-// }
-
-/**************************************************************************
- * cwrite: write routine that checks for errors and exits if an error is  *
- *         returned.                                                      *
- **************************************************************************/
-// int cwrite(int fd, char *buf, int n){
-  
-//   int nwrite;
-
-//   if((nwrite=write(fd, buf, n))<0){
-//     perror("Writing data");
-//     exit(1);
-//   }
-//   return nwrite;
-// }
-
-/**************************************************************************
- * read_n: ensures we read exactly n bytes, and puts those into "buf".    *
- *         (unless EOF, of course)                                        *
- **************************************************************************/
-// int read_n(int fd, char *buf, int n) {
-
-//   int nread, left = n;
-
-//   while(left > 0) {
-//     if ((nread = cread(fd, buf, left))==0){
-//       return 0 ;      
-//     }else {
-//       left -= nread;
-//       buf += nread;
-//     }
-//   }
-//   return n;  
-// }
-
-/**************************************************************************
- * do_debug: prints debugging stuff (doh!)                                *
- **************************************************************************/
-void do_debug(char *msg, ...){
-  
-  va_list argp;
-  
-  if (debug){
-    va_start(argp, msg);
-    vfprintf(stderr, msg, argp);
-    va_end(argp);
-  }
-}
-
-/**************************************************************************
- * my_err: prints custom error messages on stderr.                        *
- **************************************************************************/
-void my_err(char *msg, ...) {
-
-  va_list argp;
-  
-  va_start(argp, msg);
-  vfprintf(stderr, msg, argp);
-  va_end(argp);
-}
-
-/**************************************************************************
- * usage: prints usage and exits.                                         *
- **************************************************************************/
-void usage(void) {
-  fprintf(stderr, "Usage:\n");
-  fprintf(stderr, "%s -i <ifacename> [-s|-c <serverIP>] [-p <port>] [-u|-a] [-d]\n", progname);
-  fprintf(stderr, "%s -h\n", progname);
-  fprintf(stderr, "\n");
-  fprintf(stderr, "-i <ifacename>: Name of interface to use (mandatory)\n");
-  fprintf(stderr, "-s|-c <serverIP>: run in server mode (-s), or specify server address (-c <serverIP>) (mandatory)\n");
-  fprintf(stderr, "-p <port>: port to listen on (if run in server mode) or to connect to (in client mode), default 55555\n");
-  fprintf(stderr, "-u|-a: use TUN (-u, default) or TAP (-a)\n");
-  fprintf(stderr, "-d: outputs debug information while running\n");
-  fprintf(stderr, "-h: prints this help text\n");
-  exit(1);
-}
-
 int create_client_tun(char* if_name) {
   int tun_fd;
   int sock_fd;
@@ -188,6 +120,7 @@ int create_client_tun(char* if_name) {
   memset(&address, 0, sizeof(address));
   address.sin_family = AF_INET;
   address.sin_addr.s_addr = htonl(INADDR_ANY);
+  // PORT HERE 
   address.sin_port = htons(0);
   if (bind(sock_fd, (struct sockaddr*) &address, sizeof(address)) < 0) {
       perror("bind()");
